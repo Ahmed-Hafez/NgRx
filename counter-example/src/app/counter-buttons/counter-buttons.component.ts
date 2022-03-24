ff975bcfdfc8c8723fec81378b31104fe7b80067
Ahmed-Hafez <ahmed160035@feng.bu.edu.eg>
2022-03-25 00:08:14 +0200

    Adding counter example

---
+++ counter-example/src/app/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../ngrx/counter.actions';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.scss']
+})
+export class CounterButtonsComponent implements OnInit {
+
+  constructor(private store: Store<{counter: {counter: number}}>) { }
+
+  ngOnInit(): void {
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+  reset() {
+    this.store.dispatch(reset());
+  }
+
+}
